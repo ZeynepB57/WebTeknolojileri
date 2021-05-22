e2fe5c7d02e5f567373b505f6c0b2bd0239bbbb8
ZeynepB57 <77547126+ZeynepB57@users.noreply.github.com>
2021-05-23 00:19:24 +0300

    ilk

---
+++ JS/iletisim.js
@@ -0,0 +1,38 @@
+
+function kontrol()
+{
+  var adValue=document.forms["form1"]["ad"].value;
+  var soyadValue=document.forms["form1"]["soyad"].value;
+  var epostaValue=document.forms["form1"]["eposta"].value;
+  var sehirValue=document.forms["form1"]["sehir"].value;
+  
+
+  
+  if (adValue == "") {
+   alert("Ad Kısmı Boş bırakılamaz");
+   return false;
+  }
+  if (soyadValue == "") {
+  alert("soyad Kısmı Boş bırakılamaz");
+  return false;
+  }
+ 
+
+ if (epostaValue == "") {
+  alert("E-mail Kısmı Boş bırakılamaz");
+  return false;
+ }
+ else var forMail = /^[a-zA-Z0-9._-]+@([a-zA-Z0-9.-]+.)+([.])+[a-zA-Z0-9.-]{2,4}$/;
+ if (forMail.test(epostaValue) == false) {
+     alert("Geçersiz Mail Adresi!!!");
+     return false;
+ }
+
+  
+ if (sehirValue == "...") {
+  alert(" Şehir Kısmı Boş Bırakılmaz Bir Şehir Seçiniz");
+  return false;
+ }
+
+ 
+}
