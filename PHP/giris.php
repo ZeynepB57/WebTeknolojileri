a61f2d2b6fdf841775b6b21e03c5e6e15284fd68
ZeynepB57 <77547126+ZeynepB57@users.noreply.github.com>
2021-05-23 21:30:46 +0300

    düzenleme

--- PHP/giris.php
+++ PHP/giris.php
@@ -6,14 +6,15 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="CSS/hakkimda.css">
+  <link href="./style/css" rel="stylesheet">
   <!-- BOOSTRAP LİNK -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-  <title>Hakkımda</title>
+  <title>Giriş Kontrol Sayfası</title>
 </head>
 
 <body style="background-color:rgb(183, 201, 204);">
-  <header id="header">
-    <nav class="navbar fixed-top navbar-expand-md navbar-light bg-info py=3">
+  
+    <nav class="navbar navbar-expand-md navbar-light bg-info py=3">
       <div class="container">
         <h5 class="navbar-brand mb-0 " >Zeynep Bezirci</h5>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,7 +23,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Hakkımda</a>
+              <a class="nav-link" href="index.html">Hakkımda</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="ozgecmisim.html">Özgeçmişim</a>
@@ -40,7 +41,7 @@
               <a class="nav-link" href="iletisim.html">iletişim</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="giriş.html">Giriş</a>
+              <a class="nav-link active" href="giriş.html">Giriş</a>
             </li>
             
           </ul>
@@ -48,7 +49,7 @@
         </div>
       </div>
     </nav>
-  </header>
+  
 
   <div class="container mt-5 p-4 align-items-center w-75">
     <div class ="h2">HAKKIMDA</div>
